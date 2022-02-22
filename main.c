b247fd3c03749ec09e83393ac278f8b1a3c0e67c
cayque <81389290+NeonBrasil@users.noreply.github.com>
2022-02-22 23:13:32 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void) {
+char str1[20], str2[30];
+  
+printf("Digite seu nome de usuario: ");
+  scanf("%s",str1);
+  printf("Olá, %s\n", str1);
+  
+}
